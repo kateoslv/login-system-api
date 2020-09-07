08676a8e6970ff4f97111dedee92e7332ac10636
katty oliveira <kattyolv@gmail.com>
2020-09-07 22:49:27 +0100

    created package and class JDBC connection

---
+++ src/com/kattyolv/login/system/api/jdbc/ConnectionJDBC.java
@@ -0,0 +1,38 @@
+package com.kattyolv.login.system.api.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionJDBC {
+
+	private Connection connection = null;
+	private static final String USER = "root";
+	private static final String PASSWORD = "123456*";
+	private static final String URL = "jdbc:mysql://localhost:3306/login_system_api";
+	
+	public Connection getConnection() {
+	
+		if(connection == null) {
+			try {
+				connection = DriverManager.getConnection(URL, USER, PASSWORD);
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return connection;
+	}
+	
+	public void close() {
+		
+		if(connection != null) {
+			try {
+				connection.close();
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+	
+		}
+	}
+}
