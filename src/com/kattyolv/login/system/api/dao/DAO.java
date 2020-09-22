db94d649f4e7330add855296d39b18747a92e504
katty oliveira <kattyolv@gmail.com>
2020-09-22 10:21:08 +0100

    created update and delete methods

--- src/com/kattyolv/login/system/api/dao/DAO.java
+++ src/com/kattyolv/login/system/api/dao/DAO.java
@@ -12,8 +12,8 @@ public class DAO {
 
 	static final String SELECT = "SELECT * FROM users ORDER BY name";
 	static final String INSERT = "INSERT INTO users (name, email, password) VALUES (?,?,?)";
-	static final String UPDATE = "UPDATE users SET name=?, email=?, password=?";
-	static final String DELETE = "DELETE FROM users WHERE name=?";
+	static final String UPDATE = "UPDATE users SET name=?, email=?, password=? WHERE id=?";
+	static final String DELETE = "DELETE FROM users WHERE id=?";
 	
 	static Connection connection;
 	
@@ -54,6 +54,46 @@ public class DAO {
 		}
  	}
 	
+	public boolean updateData(UserModel userModel) {
+		
+		try {
+			PreparedStatement stmt = connection.prepareStatement(UPDATE);
+			
+			stmt.setString(1, userModel.getName());
+			stmt.setString(2, userModel.getEmail());
+			stmt.setString(3, userModel.getPassword());
+			stmt.setInt(4, userModel.getId());
+		
+			int rowsAffected = stmt.executeUpdate();
+			
+			if (rowsAffected > 0) {
+				return true;
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
 	
+	public int deleteData(UserModel userModel) {
+		
+		try {
+			PreparedStatement stmt = connection.prepareStatement(DELETE);
+			
+			stmt.setInt(1, userModel.getId());
+		
+			int rowsAffected = stmt.executeUpdate();
+			
+			return rowsAffected;
+			
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return 0;
+	}
 	
 }
